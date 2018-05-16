51b657f89a5ca5fdc25d016ede3edd84a524cfe8
hamajun <hmmrjn@gmail.com>
2018-05-16 09:56:50 +0900

    rails g scaffold User
    rails g scaffold User name:string email:string password_digest:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180516005300) do
+ActiveRecord::Schema.define(version: 20180516005609) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.integer "price"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
