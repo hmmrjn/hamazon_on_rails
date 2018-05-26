9ec568536de3db92287fe328068011249e4e3e70
hamajun <hmmrjn@gmail.com>
2018-05-26 23:43:53 +0900

    cart layout

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,11 @@
 class Cart < ApplicationRecord
   has_many :cart_items
+
+  def total_amount
+    total_amount = 0
+    self.cart_items.each do |cart_item|
+      total_amount += cart_item.item.price
+    end
+    total_amount
+  end
 end
