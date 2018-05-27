4c6195ef5544796c205d8d9726495537669f8c06
hamajun <hmmrjn@gmail.com>
2018-05-27 23:55:32 +0900

    score template

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,12 @@ class Item < ApplicationRecord
 
   validates :name, :price, :description, presence: true
   validates :name, uniqueness: true
+
+  def score
+    if self.reviews.present?
+      self.reviews.average(:score).floor(1).to_f
+    else
+      0
+    end
+  end
 end
